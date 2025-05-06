9038c794082679407909c710e20194951218fd75
tongue <evan.challias@tonguechaude.fr>
2025-05-06 22:36:19 +0200

    fix : clean code

--- src/main/java/Carree.java
+++ src/main/java/Carree.java
@@ -2,84 +2,59 @@ import java.awt.Color;
 
 public class Carree {
 
-	private int valeur;
+    private int valeur;
 
-	private Color couleur;
+    private Color couleur;
 
+    public Carree() {
+        valeur = 0;
+    }
 
-	public Carree() {
-		valeur = 0;
-	}
+    public Carree(int val) {
+        valeur = val;
+    }
 
-	public Carree (int val) {
-		valeur = val;
-	}
+    public int getValeur() {
+        return valeur;
+    }
 
-	public int getValeur() {
-		return valeur;
-	}
+    public Color getCouleur() {
+        this.setCouleur();
+        return couleur;
+    }
 
-	public Color getCouleur() {
-		this.setCouleur();
-		return couleur;
-	}
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
 
-	public void setValeur(int valeur) {
-		this.valeur = valeur;
-	}
+    public void setCouleur() {
+        if (valeur == 2) {
+            couleur = new Color(238, 228, 218);
+        } else if (valeur == 4) {
+            couleur = new Color(237, 224, 200);
+        } else if (valeur == 8) {
+            couleur = new Color(242, 177, 121);
+        } else if (valeur == 16) {
+            couleur = new Color(245, 149, 99);
+        } else if (valeur == 32) {
+            couleur = new Color(246, 124, 95);
+        } else if (valeur == 64) {
+            couleur = new Color(246, 94, 59);
+        } else if (valeur == 128) {
+            couleur = new Color(237, 207, 114);
+        } else if (valeur == 256) {
+            couleur = new Color(237, 204, 97);
+        } else if (valeur == 512) {
+            couleur = new Color(237, 200, 80);
+        } else if (valeur == 1024) {
+            couleur = new Color(237, 197, 63);
+        } else {
+            couleur = new Color(237, 194, 46);
+        }
+    }
 
-	public void setCouleur() {
-
-		if (valeur == 2) {
-			couleur = new Color(238, 228, 218);
-		}
-
-		else if (valeur == 4) {
-			couleur = new Color(237, 224, 200);
-		}
-
-		else if (valeur == 8) {
-			couleur = new Color(242, 177, 121);
-		}
-		else if (valeur == 16) {
-			couleur = new Color(245, 149, 99);
-		}
-
-		else if (valeur == 32) {
-			couleur = new Color(246, 124, 95);
-		}
-
-		else if (valeur == 64) {
-			couleur = new Color(246, 94, 59);
-		}
-
-		else if (valeur == 128) {
-			couleur = new Color(237, 207, 114);
-		}
-
-		else if (valeur == 256) {
-			couleur = new Color(237, 204, 97);
-		}
-
-		else if (valeur == 512) {
-			couleur = new Color(237, 200, 80);
-		}
-
-		else if (valeur == 1024) {
-			couleur = new Color(237, 197, 63);
-		}
-
-		else {
-			couleur = new Color ( 237, 194, 46 );;
-		}
-
-
-
-
-	}
-
-	@Override
-	public String toString() {
-		return  "" + valeur;
-	}
+    @Override
+    public String toString() {
+        return "" + valeur;
+    }
 }
--- src/main/java/Jeu.java
+++ src/main/java/Jeu.java
@@ -3,167 +3,134 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
-
 public class Jeu extends JPanel implements KeyListener {
 
-	TableauDeJeu t1 = new TableauDeJeu();
+    TableauDeJeu t1 = new TableauDeJeu();
 
-	static Jeu nouveauJeu = new Jeu();
+    static Jeu nouveauJeu = new Jeu();
 
-	static JFrame frame = new JFrame( "2048" );
+    static JFrame frame = new JFrame("2048");
 
-	static Color green;
+    static Color green;
 
-	String tableauDeJeu = t1.toString();
+    String tableauDeJeu = t1.toString();
 
+    public static void initGUI() {
+        frame.addKeyListener(nouveauJeu);
+        frame.getContentPane().add(nouveauJeu);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
 
-	public static void initGUI()
-	{
-		frame.addKeyListener( nouveauJeu );
-		frame.getContentPane().add( nouveauJeu );
-		frame.setSize( 600, 400);
-		frame.setVisible( true );
-		frame.setResizable( false );
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyChar() == 'z' || e.getKeyCode() == KeyEvent.VK_UP) {
+            t1.monter();
+            t1.generation();
+            tableauDeJeu = t1.toString();
+            frame.repaint();
+        } else if (
+            e.getKeyChar() == 's' || e.getKeyCode() == KeyEvent.VK_DOWN
+        ) {
+            t1.descendre();
+            t1.generation();
+            tableauDeJeu = t1.toString();
+            frame.repaint();
+        } else if (
+            e.getKeyChar() == 'q' || e.getKeyCode() == KeyEvent.VK_LEFT
+        ) {
+            t1.gauche();
+            t1.generation();
+            tableauDeJeu = t1.toString();
+            frame.repaint();
+        } else if (
+            e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT
+        ) {
+            t1.droite();
+            t1.generation();
+            tableauDeJeu = t1.toString();
+            frame.repaint();
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            t1 = new TableauDeJeu();
+            t1.generation();
+            t1.generation();
+            frame.repaint();
+        }
+    }
 
-	@Override
-	public void keyPressed ( KeyEvent e )
-	{
-		if ( e.getKeyChar() == 'z' || e.getKeyCode() == KeyEvent.VK_UP )
-		{
-			t1.monter();
-			t1.generation();
-			tableauDeJeu = t1.toString();
-			frame.repaint();
-		}
-		else if ( e.getKeyChar() == 's' || e.getKeyCode() == KeyEvent.VK_DOWN )
-		{
-			t1.descendre();
-			t1.generation();
-			tableauDeJeu = t1.toString();
-			frame.repaint();
-		}
-		else if ( e.getKeyChar() == 'q' || e.getKeyCode() == KeyEvent.VK_LEFT )
-		{
-			t1.gauche();
-			t1.generation();
-			tableauDeJeu = t1.toString();
-			frame.repaint();
-		}
-		else if ( e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT )
-		{
-			t1.droite();
-			t1.generation();
-			tableauDeJeu = t1.toString();
-			frame.repaint();
-		}
-		else if ( e.getKeyCode() == KeyEvent.VK_ENTER )
-		{
-			t1 = new TableauDeJeu();
-			t1.generation();
-			t1.generation();
-			frame.repaint();
-		}
-	}
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //sert à rien mais nécessaire
+    }
 
-	@Override
-	public void keyReleased ( KeyEvent e )
-	{
-		//sert à rien mais nécessaire
-	}
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // sert à rien mais nécessaire
+    }
 
-	@Override
-	public void keyTyped ( KeyEvent e )
-	{
-		// sert à rien mais nécessaire
-	}
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawString("2048", 250, 20);
+        g2.drawString(
+            "Score: " + t1.getScore(),
+            200 - 4 * String.valueOf(t1.getScore()).length(),
+            40
+        );
+        g2.drawString(
+            "Highest Tile: " + t1.getLeCarreeLePlusGros(),
+            280 - 4 * String.valueOf(t1.getLeCarreeLePlusGros()).length(),
+            40
+        );
+        g2.drawString("Press 'Enter' to Start", 210, 315);
+        g2.drawString("Use 'wasd' or Arrow Keys to move", 180, 335);
+        if (t1.reset()) {
+            g2.drawString("Press 'Enter' to restart", 200, 355);
+        }
+        g2.setColor(Color.gray);
+        g2.fillRect(140, 50, 250, 250);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                drawTiles(g, t1.tableau[i][j], j * 60 + 150, i * 60 + 60);
+            }
+        }
+        if (t1.partieTerminee()) {
+            g2.setColor(Color.gray);
+            g2.fillRect(140, 50, 250, 250);
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    g2.setColor(Color.RED);
+                    g2.fillRoundRect(j * 60 + 150, i * 60 + 60, 50, 50, 5, 5);
+                    g2.setColor(Color.black);
+                    g.drawString("GAME", j * 60 + 160, i * 60 + 75);
+                    g.drawString("OVER", j * 60 + 160, i * 60 + 95);
+                }
+            }
+        }
+    }
 
-	public void paint( Graphics g )
-	{
-		super.paint( g );
-		Graphics2D g2 = (Graphics2D)g;
-		g2.drawString( "2048", 250, 20 );
-		g2.drawString( "Score: " + t1.getScore(),
-				200 - 4 * String.valueOf( t1.getScore() ).length(),
-				40 );
-		g2.drawString( "Highest Tile: " + t1.getLeCarreeLePlusGros(),
-				280 - 4 * String.valueOf( t1.getLeCarreeLePlusGros() ).length(),
-				40 );
-		g2.drawString( "Press 'Enter' to Start", 210, 315 );
-		g2.drawString( "Use 'wasd' or Arrow Keys to move", 180, 335 );
-		if ( t1.reset() )
-		{
-			g2.drawString( "Press 'Enter' to restart", 200, 355 );
-		}
-		g2.setColor( Color.gray );
-		g2.fillRect( 140, 50, 250, 250 );
-		for ( int i = 0; i < 4; i++ )
-		{
-			for ( int j = 0; j < 4; j++ )
-			{
-				drawTiles( g, t1.tableau[i][j], j * 60 + 150, i * 60 + 60 );
-			}
-		}
-		if ( t1.partieTerminee() )
-		{
-			g2.setColor( Color.gray );
-			g2.fillRect( 140, 50, 250, 250 );
-			for ( int i = 0; i < 4; i++ )
-			{
-				for ( int j = 0; j < 4; j++ )
-				{
-					g2.setColor( Color.RED );
-					g2.fillRoundRect( j * 60 + 150, i * 60 + 60, 50, 50, 5, 5 );
-					g2.setColor( Color.black );
-					g.drawString( "GAME", j * 60 + 160, i * 60 + 75 );
-					g.drawString( "OVER", j * 60 + 160, i * 60 + 95 );
-				}
-			}
-		}
-	}
+    public void drawTiles(Graphics g, Carree carree, int x, int y) {
+        int tileValue = carree.getValeur();
+        int length = String.valueOf(tileValue).length();
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.lightGray);
+        g2.fillRoundRect(x, y, 50, 50, 5, 5);
+        g2.setColor(Color.black);
+        if (tileValue > 0) {
+            g2.setColor(carree.getCouleur());
+            g2.fillRoundRect(x, y, 50, 50, 5, 5);
+            g2.setColor(Color.black);
+            g.drawString("" + tileValue, x + 25 - 3 * length, y + 25);
+        }
+    }
 
-	public void drawTiles( Graphics g, Carree carree, int x, int y )
-	{
-		int tileValue = carree.getValeur();
-		int length = String.valueOf( tileValue ).length();
-		Graphics2D g2 = (Graphics2D)g;
-		g2.setColor( Color.lightGray );
-		g2.fillRoundRect( x, y, 50, 50, 5, 5 );
-		g2.setColor( Color.black );
-		if ( tileValue > 0 )
-		{
-			g2.setColor( carree.getCouleur() );
-			g2.fillRoundRect( x, y, 50, 50, 5, 5 );
-			g2.setColor( Color.black );
-			g.drawString( "" + tileValue, x + 25 - 3 * length, y + 25 );
-		}
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	public static void main(String[] args) {
-		//TableauDeJeu t1 = new TableauDeJeu();
-		//System.out.println(t1);
-		initGUI();
-	}
-
+    public static void main(String[] args) {
+        //TableauDeJeu t1 = new TableauDeJeu();
+        //System.out.println(t1);
+        initGUI();
+    }
 }
